f3987676a4ff104c7345ae8beaff552ab1bca817
Fahmi Fahreza <fahmiiireza@gmail.com>
2023-10-19 15:09:34 +0200

    Initial Commit

---
+++ drunko/drunkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  drunkoApp.swift
+//  drunko
+//
+//  Created by Fahmi Fahreza on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct drunkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
