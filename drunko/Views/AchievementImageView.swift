69e9534079a3f9254f8a0ce14896fef362e21f7a
Fahmi Fahreza <fahmiiireza@gmail.com>
2023-10-27 15:22:44 +0200

    Implement locking function for achievement

---
+++ drunko/Views/AchievementImageView.swift
@@ -0,0 +1,50 @@
+//
+//  AchievementImageView.swift
+//  drunko
+//
+//  Created by Fahmi Fahreza on 27/10/23.
+//
+
+import SwiftUI
+
+struct AchievementImageView: View {
+    let achievement: Achievement
+    @AppStorage("counterFirstDrink") var counterFirstDrink = DefaultCounters.counterFirstDrink
+    @AppStorage("counterFirstTutorial") var counterFirstTutorial = DefaultCounters.counterFirstTutorial
+    @AppStorage("counterFiveDrink") var counterFiveDrink = DefaultCounters.counterFiveDrink
+    @AppStorage("counterCoffee") var counterCoffee = DefaultCounters.counterCoffee
+    
+    var body: some View {
+        if (achievement.type == .TUTORIAL && achievement.counterEnd == counterFirstTutorial) {
+            Image(achievement.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.bottom, 30)
+        } else if ( achievement.type == .ONEDRINK && achievement.counterEnd == counterFirstDrink) {
+            Image(achievement.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.bottom, 30)
+        } else if (achievement.type == .FIVEDRINK && achievement.counterEnd == counterFiveDrink) {
+            Image(achievement.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.bottom, 30)
+        } else if (achievement.type == .FIVEDRINK && achievement.counterEnd == counterCoffee) {
+            Image(achievement.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.bottom, 30)
+        } else {
+            Image(systemName: "lock.circle")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(Color(.gray))
+                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                .padding(.bottom, 30)
+        }}
+}
+
+#Preview {
+    AchievementImageView(achievement: Achievements().achievementList[0])
+}
--- drunko/Views/AchievementView.swift
+++ drunko/Views/AchievementView.swift
@@ -8,23 +8,19 @@
 import SwiftUI
 
 struct AchievementView: View {
-    let achievements = Achievements() 
+    let achievements = Achievements()
     var body: some View {
         VStack{
             TabView{
                 ForEach(achievements.achievementList) { achievement in
                     VStack{
-                        
                         Text(achievement.title)
                             .bold()
                             .font(.title)
                             .padding(.bottom, 30)
                         
-                        Image(achievement.imageName)
-                            .resizable()
-                        //                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            .aspectRatio(contentMode: .fit)
-                            .padding(.bottom, 30)
+                        AchievementImageView(achievement: achievement)
+                        
                         
                         Text(achievement.description)
                             .padding(.bottom, 5)
